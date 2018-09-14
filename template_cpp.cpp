8fcc6e9572b0e3397467eb7e48a4b1d2f2dbdef3
vahid <vahid-heidari@hotmail.com>
2018-09-15 02:04:35 +0430

    clone git from github and add template files

---
+++ template_cpp.cpp
@@ -0,0 +1,31 @@
+/*
+	Project Name : ProjectName
+	Auhtor 		 : Your Name 
+	Git Url 	 : XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+----------------------------------------------------------
+	@File name   : template_hpp.hpp 
+*/
+
+#include <template_hpp.hpp>
+
+using namespace ProjectName;
+
+ClassName::ClassName ()
+{
+	// nothing
+
+
+//----------------------------------------------------------	
+} // ClassName::ClassName
+//==========================================================
+//----------------------------------------------------------
+
+
+ClassName::~ClassName ()
+{
+	// nothing
+
+//----------------------------------------------------------	
+} // ClassName::~ClassName
+//==========================================================
+//----------------------------------------------------------
---
+++ template_hpp.hpp
@@ -0,0 +1,41 @@
+/*
+	Project Name : ProjectName
+	Auhtor 		 : Your Name
+	Git Url 	 : XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+----------------------------------------------------------
+	@File name   : template_hpp.hpp 
+*/
+
+#ifndef TEMPLATE_HPP_HPP
+#define TEMPLATE_HPP_HPP
+
+
+//---------------------------------------------------------
+namespace ProjectName {
+
+class ClassName {
+
+	public:
+		ClassName ();
+		virtual ~ClassName ();
+	
+
+	private:
+		// nothing
+
+	protected:
+		// nothing
+
+//----------------------------------------------------------	
+}; // ClassName Class
+//==========================================================
+//----------------------------------------------------------
+
+//----------------------------------------------------------	
+} //  ProjectName namespace
+//==========================================================
+//----------------------------------------------------------
+
+
+
+#endif
